17d28cbcf159c55262613e355d462780f9550c3a
samuel <samuelqlopes99@gmail.com>
2025-06-03 11:22:44 -0300

    add explanation of how Transformer work

--- ast_builder.py
+++ ast_builder.py
@@ -5,7 +5,7 @@ from enum import Enum
 # grammar
 from grammar import cuda_grammar
 
-# class for the Node of the Abstract Syntax Tree
+# classes for the Nodes of the Abstract Syntax Tree. So each class is an Object that will be a node when called.
 class ASTNode:
     #pass
     def pretty_print(self, indent=0):
@@ -256,23 +256,45 @@ __global__ void vecAdd(int* a, int* b, int* c) {
 
 parser = Lark(cuda_grammar)
 tree = parser.parse(kernel_vecAdd)
-print(tree.pretty())
+print(f"parse tree: {tree}")
+#print(tree.pretty())
 
 x = CUDA_AST()
 ast = x.transform(tree)
 #print(ast.pretty())
 
-print(f"type ast: {type(ast)}")
-print(f"params: {ast.parameters}")
-print(f"body: {ast.body}")
-
-ast.pretty_print()
+#ast.pretty_print() # structured print
 
 # "binary" = 2 operands and 1 operator: 2 + 3 or 2 + (3 * 4) or a + b
 # "literal" = constant value: 2 or 3.1 or "string"
 # "variable" = a named identifier: a or result or threadIdx
 
-# Obs: in ASTs, the interior nodes are the operators (+, *, -, /, ...) And the leaves are the operands (2, 6, 1, ...)
 # Expression is "Term((+|-) Term)*" . Ex: (8 + 24 * 2) is an expression because there are one Term '8' + another Term '24 * 2'
 # Term is "Factor((*|/) Factor)*" Ex: (8) + (24 * 2), so 8 is a term and (24 * 2) is another term, because they are splitted by + signal
 # Factor are the numbers. Ex: 8 + 24 * 2, so 8, 24, 2 are all factors
+
+
+# Transformer funcionality:
+# In parse tree only has Tree() or Token(). Tree(data=rule_name, children=[...]) while Token(type=TERMINAL_SYMBOL, value). 
+# Tree() represent grammar rules. Token() represent terminal symbols (leaves).
+#
+# Imagine we have:
+# Tree(Token('RULE', 'qualifier'), [Token('QUALIFIER', '__global__')])
+# 1) - data field is a Token('RULE', 'qualifier'), which means that the rule_name is 'qualifier'
+#    - has one child [Token('QUALIFIER', '__global__')], which is a terminal token with the string '__global__'
+#
+# 2) Transformer sees the Tree(...)
+#    - extracts the rule_name from data which is "qualifier"
+#    - it looks for a method in Transformer class called "def qualifier()"
+#
+# 3) Before calling the method, the Transformer recursively transforms the children
+#    - the child is a Token(), so converts to its string value which is "__global__"
+#    - after doing this with all the children, it calls the qualifier method passing the children as parameters
+#      so: qualifier(self, ["__global__"])
+# 
+# 4) The qualifier method receives ["__global__"]:
+#    - then it can create an AST node calling the corresponded class "Qualifier(...)"  or simply returns the string.
+#
+# When to return a string and when to create a AST node?
+# Node: when represents a semantic structure. is not just a string, but a construct with meaning and behavior
+# string: when is just a primitive value like: "int", 'x', "__global__", ...
