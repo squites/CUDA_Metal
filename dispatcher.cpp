9dd31185a8ef6d948e124fd618a714b7603d12d9
samuel <samuelqlopes99@gmail.com>
2025-07-26 02:35:55 -0300

    cleaned code

--- dispatcher.cpp
+++ dispatcher.cpp
@@ -13,93 +13,61 @@ int main() {
             
     // 1) get device
     MTL::Device* device = MTL::CreateSystemDefaultDevice();
-    if (!device) {
-        std::cerr<<"Failed to create device"<<std::endl;
-        return -1;
-    }
 
     // 2) create command queue
     MTL::CommandQueue* commandQueue = device->newCommandQueue();
-    if (!commandQueue) {
-        std::cerr<<"Failed to create command queue"<<std::endl;
-        device->release();
-        return -1;
-    }
 
-    // 3) load metal library and create the function
+    // 3) load metal library
     NS::String* filePath = NS::String::string("./vecAdd.metallib", NS::ASCIIStringEncoding);
     MTL::Library* library = device->newLibrary(filePath, &error);
-    if (!library) {
-        std::cerr << "Failed to create library: " << error->localizedDescription()->utf8String() << "\n";
-        filePath->release();
-        commandQueue->release();
-        device->release();
-        return -1;
-    }
 
-    
+    // 4) create function 
     MTL::Function* function = library->newFunction(NS::String::string("vecAdd", NS::ASCIIStringEncoding));
-    if (!function) {
-        std::cerr<<"Failed to create a function"<<std::endl;
-        library->release();
-        commandQueue->release();
-        device->release();
-        return -1;
-    }
 
-    // 4) create compute pipeline state
+    // 5) create compute pipeline state
     MTL::ComputePipelineState* pipelineState = device->newComputePipelineState(function, &error);
-    if (!pipelineState) {
-        std::cerr << "Failed to create pipeline state: " << error->localizedDescription()->utf8String() << "\n";
-        function->release();
-        library->release();
-        commandQueue->release();
-        device->release();
-        return -1;
-    }
 
-    // 5) prepare input data
+    // 6) prepare input data
     float* dataA = new float[arrayLength];
     float* dataB = new float[arrayLength];
 
-    // fill arrays with sample data
+    // fill arrays with data (todo: create func to generate random float numbers)
     for (int i = 0; i < arrayLength; i++) {
         dataA[i] = static_cast<float>(i);
         dataB[i] = static_cast<float>(i*2);
     }
 
-    // 6) create metal buffers
+    // 7) create metal buffers
     MTL::Buffer* bufferA = device->newBuffer(dataA, arrayLength * sizeof(float), MTL::ResourceStorageModeShared);
     MTL::Buffer* bufferB = device->newBuffer(dataB, arrayLength * sizeof(float), MTL::ResourceStorageModeShared);
     MTL::Buffer* bufferResult = device->newBuffer(arrayLength * sizeof(float), MTL::ResourceStorageModeShared);
 
-    // 7) create command buffer and compute encoder
+    // 8) create command buffer and compute encoder
     MTL::CommandBuffer* commandBuffer = commandQueue->commandBuffer();
     MTL::ComputeCommandEncoder* computeEncoder = commandBuffer->computeCommandEncoder();
 
-    // 8) set pipeline state and buffers
+    // 9) set pipeline state and buffers
     computeEncoder->setComputePipelineState(pipelineState);
     computeEncoder->setBuffer(bufferA, 0, 0);
     computeEncoder->setBuffer(bufferB, 0, 1);
     computeEncoder->setBuffer(bufferResult, 0, 2);
 
-    // 9) calculate grid and threadgroup sizes
+    // 10) calculate grid and threadgroup sizes
     MTL::Size gridSize = MTL::Size(arrayLength, 1, 1);
     MTL::Size threadgroupSize = MTL::Size(pipelineState->maxTotalThreadsPerThreadgroup(), 1, 1);
 
-    // 10) dispatch threads
+    // 11) dispatch threads
     computeEncoder->dispatchThreads(gridSize, threadgroupSize);
 
-    // 11) end encoding, commit command buffer
+    // 12) end encoding, commit command buffer
     computeEncoder->endEncoding();
     commandBuffer->commit();
 
-    // 12) wait for complete
+    // 13) wait for complete
     commandBuffer->waitUntilCompleted();
 
-    // 13) verify results
+    // verify results
     float* resultData = static_cast<float*>(bufferResult->contents());
-    //print result
     std::cout<<"\ndataA:\n";
     for (int i = 0; i < arrayLength; i++) {
         std::cout<<dataA[i]<<"  ";
@@ -113,7 +81,7 @@ int main() {
         std::cout<<resultData[i]<<"  ";
     }
 
-    // After verifying results, release resources
+    // release resources (free)
     bufferA->release();
     bufferB->release();
     bufferResult->release();
